a75db0f00039e0f880aa20d349c4e8020a6400b8
Ranomenjanahary Miora <gabrielle.ranomenjanahary@gmail.com>
2021-04-07 20:22:28 +0300

    third commit

--- src/app/assignments/assignment-detail/assignment-detail.component.ts
+++ src/app/assignments/assignment-detail/assignment-detail.component.ts
@@ -32,7 +32,7 @@ export class AssignmentDetailComponent implements OnInit {
     
     this.isUser();
     this.getAssignmentById();
-    this.arrayNumbers=[0,1,2,3,4,5,6,7,8,3,10,11,12,13,14,15,16,17,18,19];
+    this.arrayNumbers=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
     console.log("Role:"+this.isUser());  
   }
 
